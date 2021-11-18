fd4f19d55ecfe65d36093dd590852fea676fb6ea
kate <tbtehf12@naver.com>
2021-11-18 09:52:11 -0500

    Implmented Conntections

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,22 @@
-alert("Hello");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// This will only be executed when the socket open the connection
+// This will be displayed in console on web
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+}); 
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message: ", message.data);
+});
+
+// Server connection lost
+socket.addEventListener("close", () => {
+    console.log("Disconnected from the Server ❌");
+});
+
+// Send message to the Back-End
+// To prevent send a message immediately / wait for 10 sec
+setTimeout(() => {
+    socket.send("Hello from the Browser");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -14,11 +14,22 @@ const handleListen = () => console.log(`Listening on http://locahost:3000`);
 // app.listen(3000, handleListen); 
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
+
+// This wil be displayed in the terminal
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    socket.send("Hello");
+    socket.on("message", (message) => {
+        console.log(message.toString('utf-8'));
+    });
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+});
 
 server.listen(3000, handleListen);
 
 // server can handle ws, https by implementing WebSockets
 // Reasons to be needed http server: 
-// want views, static files, home, redirection
+// want views, static files, home, redirection`
+
+// socket is a connection between me and browser
