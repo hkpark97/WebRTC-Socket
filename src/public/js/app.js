c6018aa093e03b9101ea6359d6988d5db38fe6bf
kate <tbtehf12@naver.com>
2021-11-18 11:49:40 -0500

    Send message cross-platform

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,22 +1,34 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 // This will only be executed when the socket open the connection
 // This will be displayed in console on web
 socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-}); 
+  console.log("Connected to Server ✅");
+});
 
 socket.addEventListener("message", (message) => {
-    console.log("New Message: ", message.data);
+  console.log("New Message: ", message.data);
 });
 
 // Server connection lost
 socket.addEventListener("close", () => {
-    console.log("Disconnected from the Server ❌");
+  console.log("Disconnected from the Server ❌");
 });
 
 // Send message to the Back-End
 // To prevent send a message immediately / wait for 10 sec
-setTimeout(() => {
-    socket.send("Hello from the Browser");
-}, 10000);
+// setTimeout(() => {
+//     socket.send("Hello from the Browser");
+// }, 10000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  // send something to the back-end
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -11,25 +11,31 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://locahost:3000`);
-// app.listen(3000, handleListen); 
+// app.listen(3000, handleListen);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// dummy DB
+const sockets = [];
+
 // This wil be displayed in the terminal
 wss.on("connection", (socket) => {
-    console.log("Connected to Browser ✅");
-    socket.send("Hello");
-    socket.on("message", (message) => {
-        console.log(message.toString('utf-8'));
-    });
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  // put dummy DB into socket
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("message", (message) => {
+    console.log(message.toString("utf-8"));
+    // mark each browser as aSocket then, send message
+    sockets.forEach(aSocket => aSocket.send(message.toString()));
+  });
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
 });
 
 server.listen(3000, handleListen);
 
 // server can handle ws, https by implementing WebSockets
-// Reasons to be needed http server: 
+// Reasons to be needed http server:
 // want views, static files, home, redirection`
 
 // socket is a connection between me and browser
