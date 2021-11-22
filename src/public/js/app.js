cbc254f5bdc5d5132113c34e3525573826f68259
kate <tbtehf12@naver.com>
2021-11-22 15:23:22 -0500

    added Web RTC

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,94 +1,264 @@
 // io function will automatically search the server that running socket.io
 const socket = io();
 
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const call = document.getElementById("call");
 
-room.hidden = true;
+call.hidden = true;
 
+let myStream;
+let muted = false;
+let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message; 
-  ul.appendChild(li);
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    // checking which camera is being used
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function handleMessageSubmit(event) {
+async function getMedia(deviceId) {
+  // when user have no device ID
+  const initialConstraints = {
+    audio: true,
+    vide: { facingMode: "user" },
+  };
+  // execute when the user have device ID
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
+    myFace.srcObject = myStream;
+    //  To prevent growing the number of camera
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// getMedia();
+
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+function handleCameraChange() {
+  getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  // calling all the medias(camera, mic, etc.)
+  await getMedia();
+  makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
   input.value = "";
 }
 
-function handleNickNameSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#name input");
-  socket.emit("nickname", input.value);
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// socket code
+// Generate peer A
+socket.on("Welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("Sent the Offer");
+  socket.emit("offer", offer, roomName);
+});
+
+// Generate peer B
+socket.on("offer", async (offer) => {
+  console.log("Received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("Sent the answer");
+});
+
+socket.on("answer", (answer) => {
+  console.log("Received the answer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("Received Candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
 
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgForm  = room.querySelector("#msg");
-  const nameForm = room.querySelector("#name");
-  // send function to the back-end
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  nameForm.addEventListener("submit", handleNickNameSubmit);
+function handleIce(data) {
+  console.log("Sent Candidate");
+  socket.emit("ice", data.candidate, roomName);
 }
 
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = ""; 
-} 
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream;
+}
 
-form.addEventListener("submit", handleRoomSubmit);
+// constraints: what we want to get
 
-// execute socket.to function
-socket.on("Welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user} just joined!`);
-});
+// myStream: combined with video, audio from the user
 
-socket.on("Bye", (left, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${left} left`);
-});
+// const welcome = document.getElementById("welcome");
+// const form = welcome.querySelector("form");
+// const room = document.getElementById("room");
 
-// receiving message
-socket.on("new_message", addMessage);
+// room.hidden = true;
 
-// notify everyone in the room
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  // if there's a empty room clear the status
-  roomList.innerHTML = ""; 
-  if(rooms.length === 0 ) {
-    return;
-  }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.appendChild(li);
-  })
-});
+// let roomName;
 
+// function addMessage(message) {
+//   const ul = room.querySelector("ul");
+//   const li = document.createElement("li");
+//   li.innerText = message;
+//   ul.appendChild(li);
+// }
 
-// The appendChild() method of the Node interface adds a node to the end of 
-// the list of children of a specified parent node. 
-// If the given child is a reference to an existing node in the document, 
-// appendChild() moves it from its current position to the new position 
-// (there is no requirement to remove the node from its parent node 
-// before appending it to some other node).
+// function handleMessageSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#msg input");
+//   const value = input.value;
+//   socket.emit("new_message", input.value, roomName, () => {
+//     addMessage(`You: ${value}`);
+//   });
+//   input.value = "";
+// }
+
+// function handleNickNameSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#name input");
+//   socket.emit("nickname", input.value);
+// }
+
+// function showRoom() {
+//   welcome.hidden = true;
+//   room.hidden = false;
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room ${roomName}`;
+//   const msgForm  = room.querySelector("#msg");
+//   const nameForm = room.querySelector("#name");
+//   // send function to the back-end
+//   msgForm.addEventListener("submit", handleMessageSubmit);
+//   nameForm.addEventListener("submit", handleNickNameSubmit);
+// }
+
+// function handleRoomSubmit(event) {
+//   event.preventDefault();
+//   const input = form.querySelector("input");
+//   socket.emit("enter_room", input.value, showRoom);
+//   roomName = input.value;
+//   input.value = "";
+// }
+
+// form.addEventListener("submit", handleRoomSubmit);
+
+// // execute socket.to function
+// socket.on("Welcome", (user, newCount) => {
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room ${roomName} (${newCount})`;
+//   addMessage(`${user} just joined!`);
+// });
+
+// socket.on("Bye", (left, newCount) => {
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room ${roomName} (${newCount})`;
+//   addMessage(`${left} left`);
+// });
+
+// // receiving message
+// socket.on("new_message", addMessage);
+
+// // notify everyone in the room
+// socket.on("room_change", (rooms) => {
+//   const roomList = welcome.querySelector("ul");
+//   // if there's a empty room clear the status
+//   roomList.innerHTML = "";
+//   if(rooms.length === 0 ) {
+//     return;
+//   }
+//   rooms.forEach((room) => {
+//     const li = document.createElement("li");
+//     li.innerText = room;
+//     roomList.appendChild(li);
+//   })
+// });
+
+// // The appendChild() method of the Node interface adds a node to the end of
+// // the list of children of a specified parent node.
+// // If the given child is a reference to an existing node in the document,
+// // appendChild() moves it from its current position to the new position
+// // (there is no requirement to remove the node from its parent node
+// // before appending it to some other node).
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -11,96 +10,117 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://locahost:3000`);
-// app.listen(3000, handleListen);
-
 const httpServer = http.createServer(app);
-
-// Socket admin panel
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-
-instrument(wsServer, {
-  auth: false,
-});
-
-// getting sids and rooms inside of wsServer
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  // finding public rooms
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  // if precondition is true return .size else return undefined
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
+const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    // make chat room
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    // execute function showRoom
-    done();
-    // send a message to one room
-    socket.to(roomName).emit("Welcome", socket.nickname, countRoom(roomName));
-    // notify everyone in the room
-    wsServer.sockets.emit("room_change", publicRooms());
+    socket.to(roomName).emit("Welcome");
   });
-  // send a message when someone is leaving
-  socket.on("disconnecting", () => {
-    // countRoom(room) -1 => we haven't left the room so they will count include you, so we instead do -1
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("Bye", socket.nickname, countRoom(room) - 1)
-    );
+  socket.on("offer", (offer, roomName) => {
+    socket.on(roomName).emit("offer", offer);
   });
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
+  socket.on("answer", (answer, roomName) => {
+    socket.on(roomName).emit("answer", answer);
   });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    // this will be executed on the front-end side
-    done();
+  socket.on("ice", (ice, roomName) => {
+    socket.on(roomName).emit("ice", ice);
   });
-  // getting nick name
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
-// disconnect: firing when the connection is completely lost (room info is empty)
-// disconnecting: browser is closed but the server is still connecting. (room info is valid)
-
+const handleListen = () => console.log(`Listening on http://locahost:3000`);
 httpServer.listen(3000, handleListen);
 
-{
-  type: "message";
-  payload: "Hello";
-}
+// app.listen(3000, handleListen);
 
-{
-  type: "nickname";
-  payload: "Kate Park";
-}
+// const httpServer = http.createServer(app);
 
-// The JSON.stringify() method converts a JavaScript object or value to a JSON string
-// => Convert object to string
+// // Socket admin panel
+// const wsServer = new Server(httpServer, {
+//   cors: {
+//     origin: ["https://admin.socket.io"],
+//     credentials: true,
+//   },
+// });
 
-// The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string. An optional reviver function can be provided to perform a transformation on the resulting object before it is returned.
-// => Convert string to JS object
+// instrument(wsServer, {
+//   auth: false,
+// });
+
+// // getting sids and rooms inside of wsServer
+// function publicRooms() {
+//   const {
+//     sockets: {
+//       adapter: { sids, rooms },
+//     },
+//   } = wsServer;
+//   // finding public rooms
+//   const publicRooms = [];
+//   rooms.forEach((_, key) => {
+//     if (sids.get(key) === undefined) {
+//       publicRooms.push(key);
+//     }
+//   });
+//   return publicRooms;
+// }
+
+// function countRoom(roomName) {
+//   // if precondition is true return .size else return undefined
+//   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+// }
+
+// wsServer.on("connection", (socket) => {
+//   socket["nickname"] = "Anon";
+//   socket.onAny((event) => {
+//     console.log(`Socket Event: ${event}`);
+//   });
+//   socket.on("enter_room", (roomName, done) => {
+//     // make chat room
+//     socket.join(roomName);
+//     // execute function showRoom
+//     done();
+//     // send a message to one room
+//     socket.to(roomName).emit("Welcome", socket.nickname, countRoom(roomName));
+//     // notify everyone in the room
+//     wsServer.sockets.emit("room_change", publicRooms());
+//   });
+//   // send a message when someone is leaving
+//   socket.on("disconnecting", () => {
+//     // countRoom(room) -1 => we haven't left the room so they will count include you, so we instead do -1
+//     socket.rooms.forEach((room) =>
+//       socket.to(room).emit("Bye", socket.nickname, countRoom(room) - 1)
+//     );
+//   });
+//   socket.on("disconnect", () => {
+//     wsServer.sockets.emit("room_change", publicRooms());
+//   });
+//   socket.on("new_message", (msg, room, done) => {
+//     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+//     // this will be executed on the front-end side
+//     done();
+//   });
+//   // getting nick name
+//   socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+// });
+
+// // disconnect: firing when the connection is completely lost (room info is empty)
+// // disconnecting: browser is closed but the server is still connecting. (room info is valid)
+
+// httpServer.listen(3000, handleListen);
+
+// {
+//   type: "message";
+//   payload: "Hello";
+// }
+
+// {
+//   type: "nickname";
+//   payload: "Kate Park";
+// }
+
+// // The JSON.stringify() method converts a JavaScript object or value to a JSON string
+// // => Convert object to string
+
+// // The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string. An optional reviver function can be provided to perform a transformation on the resulting object before it is returned.
+// // => Convert string to JS object
